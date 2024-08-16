12c347f2eee6cf4e7d8d1cbf3e467de52914061d
Roman Ratsun <roman.ratsun@atrocore.com>
2024-08-16 12:09:18 +0000

    .

--- app/Services/AbstractExportType.php
+++ app/Services/AbstractExportType.php
@@ -345,6 +345,7 @@ abstract class AbstractExportType extends Base
         $params['offset'] = $offset;
         $params['maxSize'] = $this->data['limit'];
         $params['withDeleted'] = !empty($this->data['feed']['data']['withDeleted']);
+        $params['__callbacks'][] = [$this, 'queryCallback'];
 
         if (!empty($this->data['feed']['sortOrderField'])) {
             $params['sortBy'] = $this->data['feed']['sortOrderField'];
