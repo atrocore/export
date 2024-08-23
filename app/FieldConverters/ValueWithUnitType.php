46f0c19846e3bb8d59f2d9b679cdc074a15e24d0
Roman Ratsun <roman.ratsun@atrocore.com>
2024-08-23 07:24:01 +0000

    .

--- app/FieldConverters/ValueWithUnitType.php
+++ app/FieldConverters/ValueWithUnitType.php
@@ -32,56 +32,77 @@ class ValueWithUnitType extends AbstractType
             $attributeId = $configuration['attributeId'];
         }
 
-        $prepareUnitValueFnc = function (string $value): array {
-            $valueParts = explode('::atro::', $value);
-            return [
-                $valueParts[0] === 'N/A' ? null : (float)$valueParts[0],
-                $valueParts[1] === 'N/A' ? null : (float)$valueParts[1],
-                !empty($valueParts[2]) && $valueParts[2] === 'N/A' ? null : $valueParts[2]
-            ];
-        };
-
         if (empty($attributeId)) {
-            $fieldDefs = $this->getMetadata()->get(['entityDefs', $configuration['entity'], 'fields', $configuration['field']]);
+            $fieldDefs = $this
+                ->getMetadata()
+                ->get(['entityDefs', $configuration['entity'], 'fields', $configuration['field']]);
             // use main field instead
             $field = $fieldDefs['mainField'];
             $type = $this->getMetadata()->get(['entityDefs', $configuration['entity'], 'fields', $field, 'type']);
-            $valueResult = $this->convertor->convertType($type, $record, array_merge($configuration, ['field' => $field]))[$column];
+            $valueResult = $this
+                ->convertor
+                ->convertType($type, $record, array_merge($configuration, ['field' => $field]))[$column];
             $unitResult = $record[$field . 'UnitName'] ?? '';
         } else {
             $attribute = $this->convertor->getAttributeById($attributeId);
             $field = $configuration['field'];
             $type = $attribute->get('type');
 
-            if (!empty($record[$field])) {
-                list($valueFrom, $valueTo, $unitId) = $prepareUnitValueFnc((string) $record[$field]);
-            } else {
-                $valueFrom = null;
-                $unitId = null;
+            $valueFrom = null;
+            $valueTo = null;
+            $unitResult = '';
+
+            if ($configuration['entity'] === 'ProductAttributeValue') {
+                $valueFrom = $record['valueFrom'] ?? $record['value'];
+                $valueTo = $record['valueTo'] ?? null;
+                $unitResult = $record['valueUnitData']['name'] ?? '';
+            } elseif ($configuration['entity'] === 'Product') {
+                if (!empty($record[$field])) {
+                    list($valueFrom, $valueTo, $unitId) = $this->prepareUnitValue((string)$record[$field]);
+                    $unitResult = $this->convertor
+                        ->convertType(
+                            'unit',
+                            ["{$field}Id" => $unitId],
+                            array_merge($configuration, [
+                                    'field'               => $field,
+                                    'exportBy'            => ['name'],
+                                    'markForNoRelation'   => '',
+                                    'prepareUnitValueFnc' => [$this, 'prepareUnitValue']
+                                ]
+                            ))[$column];
+                }
             }
 
-            $valueResult = $this->convertor->convertType($type, [$field => $valueFrom], array_merge($configuration, ['field' => $field]))[$column];
-            $unitResult = $this->convertor->convertType('unit', ["{$field}Id" => $unitId], array_merge($configuration, ['field' => $field, 'exportBy' => ['name'], 'markForNoRelation' => '', 'prepareUnitValueFnc' => $prepareUnitValueFnc]))[$column];
+            $valueResult = $this->convertor
+                ->convertType($type, [$field => $valueFrom], array_merge($configuration, ['field' => $field]))[$column];
         }
 
         if (in_array($type, ['rangeFloat', 'rangeInt'])) {
             $type = $type === 'rangeFloat' ? 'float' : 'int';
             if (empty($attributeId)) {
-                $valueFromResult = $this->convertor->convertType($type, $record, array_merge($configuration, ['field' => $field . 'From']))[$column];
-                $valueToResult = $this->convertor->convertType($type, $record, array_merge($configuration, ['field' => $field . 'To']))[$column];
-            }else{
+                $valueFromResult = $this->convertor
+                    ->convertType($type, $record, array_merge($configuration, ['field' => $field . 'From']))[$column];
+                $valueToResult = $this->convertor
+                    ->convertType($type, $record, array_merge($configuration, ['field' => $field . 'To']))[$column];
+            } else {
                 $valueFromResult = $valueResult;
-                $valueToResult = $this->convertor->convertType($type, [$field => $valueTo], array_merge($configuration, ['field' => $field]))[$column];
+                $valueToResult = $this->convertor
+                    ->convertType($type, [$field => $valueTo],
+                        array_merge($configuration, ['field' => $field]))[$column];
             }
 
             $result[$column] = "";
 
             if (!$this->isNullorEmptyResult($valueFromResult) && !$this->isNullorEmptyResult($valueToResult)) {
                 $result[$column] = "$valueFromResult - $valueToResult";
-            } else if (!$this->isNullorEmptyResult($valueFromResult)) {
-                $result[$column] = ">= $valueFromResult";
-            } else if (!$this->isNullorEmptyResult($valueToResult)) {
-                $result[$column] = "<= $valueToResult";
+            } else {
+                if (!$this->isNullorEmptyResult($valueFromResult)) {
+                    $result[$column] = ">= $valueFromResult";
+                } else {
+                    if (!$this->isNullorEmptyResult($valueToResult)) {
+                        $result[$column] = "<= $valueToResult";
+                    }
+                }
             }
         } else {
             $result[$column] = "$valueResult";
@@ -95,6 +116,16 @@ class ValueWithUnitType extends AbstractType
     public function isNullorEmptyResult(string $result = null): bool
     {
         return in_array($result, [$this->nullValue, $this->emptyValue]);
+    }
+
+    public function prepareUnitValue(string $value): array
+    {
+        $valueParts = explode('::atro::', $value);
+        return [
+            $valueParts[0] === 'N/A' ? null : (float)$valueParts[0],
+            $valueParts[1] === 'N/A' ? null : (float)$valueParts[1],
+            !empty($valueParts[2]) && $valueParts[2] === 'N/A' ? null : $valueParts[2]
+        ];
     }
 
     protected function prepareQueryCallbackForAttribute(QueryBuilder $qb, array $conf, string $alias): void
