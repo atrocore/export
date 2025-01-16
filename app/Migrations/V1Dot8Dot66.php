261d10156d9c5a94c07fc9ec555277714cebc566
Roman Ratsun <roman.ratsun@atrocore.com>
2025-01-16 13:14:30 +0000

    .

--- app/Migrations/V1Dot8Dot66.php
+++ app/Migrations/V1Dot8Dot66.php
@@ -12,27 +12,111 @@
 namespace Export\Migrations;
 
 use Atro\Core\Migration\Base;
+use Doctrine\DBAL\ParameterType;
 
 class V1Dot8Dot66 extends Base
 {
     public function getMigrationDateTime(): ?\DateTime
     {
-        return new \DateTime('2025-01-14 18:00:00');
+        return new \DateTime('2025-01-16 18:00:00');
     }
 
     public function up(): void
     {
-        // ALTER TABLE export_feed ADD locale_id VARCHAR(36) DEFAULT NULL;
-        // ALTER TABLE export_feed DROP language;
-        // ALTER TABLE export_feed DROP fallback_language;
-        // CREATE INDEX IDX_EXPORT_FEED_LOCALE_ID ON export_feed (locale_id, deleted)
+        $this->execute("ALTER TABLE export_feed ADD locale_id VARCHAR(36) DEFAULT NULL");
+        $this->execute("CREATE INDEX IDX_EXPORT_FEED_LOCALE_ID ON export_feed (locale_id, deleted)");
 
-        // 'internal' -> 'name'
+        $this->getConnection()->createQueryBuilder()
+            ->update('export_configurator_item')
+            ->set('column_type', ':nameType')
+            ->where('column_type=:internalType')
+            ->setParameter('nameType', 'name')
+            ->setParameter('internalType', 'internal')
+            ->executeQuery();
 
-        // create new locale according to feeds configurations
-        // thousandSeparator
-        // decimalMark
-        // language
-        // fallbackLanguage
+        try {
+            $exportFeeds = $this->getConnection()->createQueryBuilder()
+                ->select('*')
+                ->from('export_feed')
+                ->where('deleted=:false')
+                ->setParameter('false', false, ParameterType::BOOLEAN)
+                ->fetchAllAssociative();
+        } catch (\Throwable $e) {
+            $exportFeeds = [];
+        }
+
+        $newLocales = [];
+
+        foreach ($exportFeeds as $exportFeed) {
+            $data = @json_decode($exportFeed['data'], true);
+
+            $language = $exportFeed['language'] ?? $this->getConfig()->get('mainLanguage', 'en_US');
+            $decimalMark = $data['feedFields']['decimalMark'] ?? '.';
+            $thousandSeparator = $data['feedFields']['thousandSeparator'] ?? '';
+
+            $id = substr(md5("{$language}_{$decimalMark}_$thousandSeparator"), 0, 36);
+
+            $newLocales[$id] = [
+                'id'                => $id,
+                'languageCode'      => $language,
+                'decimalMark'       => $decimalMark,
+                'thousandSeparator' => $thousandSeparator
+            ];
+
+            $this->getConnection()->createQueryBuilder()
+                ->update('export_feed')
+                ->set('locale_id', ':localeId')
+                ->where('id=:id')
+                ->setParameter('localeId', $id)
+                ->setParameter('id', $exportFeed['id'])
+                ->executeQuery();
+        }
+
+        if (!empty($newLocales)) {
+            if (!is_dir('data/reference-data')) {
+                mkdir('data/reference-data');
+            }
+
+            if (file_exists('data/reference-data/Locale.json')) {
+                $locales = @json_decode(@file_get_contents('data/reference-data/Locale.json'), true);
+            }
+
+            if (empty($locales)) {
+                $locales = [];
+            }
+
+            $number = 1;
+            foreach ($newLocales as $newLocale) {
+                if (isset($locales[$newLocale['id']])) {
+                    continue;
+                }
+                $locales[$newLocale['id']] = [
+                    'id'                => $id,
+                    'name'              => "Locale $number",
+                    'code'              => $id,
+                    'timeFormat'        => 'HH:mm',
+                    'weekStart'         => 'sunday',
+                    'dateFormat'        => 'MM\/DD\/YYYY',
+                    'timeZone'          => 'UTC',
+                    'createdAt'         => date('Y-m-d H:i:s'),
+                    'modifiedAt'        => date('Y-m-d H:i:s'),
+                    'createdById'       => 'system',
+                    'languageCode'      => $language,
+                    'decimalMark'       => $decimalMark,
+                    'thousandSeparator' => $thousandSeparator
+                ];
+                $number++;
+            }
+
+            file_put_contents('data/reference-data/Locale.json', json_encode($locales));
+        }
+    }
+
+    protected function execute(string $query): void
+    {
+        try {
+            $this->getPDO()->exec($query);
+        } catch (\Throwable $e) {
+        }
     }
 }
