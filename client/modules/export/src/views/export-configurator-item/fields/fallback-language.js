692933274acb77e07b3a6b08962a8cb83dc4547a
Roman Ratsun <roman.ratsun@atrocore.com>
2025-01-20 07:32:50 +0000

    .

--- client/modules/export/src/views/export-configurator-item/fields/fallback-language.js
+++ client/modules/export/src/views/export-configurator-item/fields/fallback-language.js
@@ -8,25 +8,45 @@
  * @license    GPLv3 (https://www.gnu.org/licenses/)
  */
 
-Espo.define('export:views/export-configurator-item/fields/fallback-language', 'export:views/export-feed/fields/fallback-language',
+Espo.define('export:views/export-configurator-item/fields/fallback-language', 'export:views/export-feed/fields/language',
     Dep => {
         return Dep.extend({
-            setup(){
+
+            prohibitedEmptyValue: false,
+
+            setup() {
                 Dep.prototype.setup.call(this)
 
                 this.listenTo(this.model, 'change:_isMultilang', () => {
                     this.checkFieldVisibility()
                 });
             },
-            afterRender(){
+
+            afterRender() {
                 Dep.prototype.afterRender.call(this);
 
                 if (this.mode !== 'list') {
                     this.checkFieldVisibility();
                 }
             },
-            checkFieldVisibility() {
 
+            setupOptions() {
+                if (this.model.get('language') === "main") {
+                    this.params.options = [];
+                    this.translatedOptions = {};
+                } else {
+                    this.params.options = ['main'];
+                    this.translatedOptions = {"main": this.getLanguage().translateOption('main', 'languageFilter', 'Global')};
+                }
+
+                (this.getConfig().get('inputLanguageList') || []).forEach(locale => {
+                    if (locale === this.model.get('language')) return;
+                    this.params.options.push(locale);
+                    this.translatedOptions[locale] = locale;
+                });
+            },
+
+            checkFieldVisibility() {
                 if (this.model.get('_isMultilang') && (this.getConfig().get('inputLanguageList') || []).length > 0) {
                     this.show();
                 } else {
