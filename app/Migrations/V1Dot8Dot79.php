0959ffdac191d5e5ffbc3ecb59b37e69969ab505
Roman Ratsun <roman.ratsun@atrocore.com>
2025-04-15 09:44:11 +0000

    .

--- app/Migrations/V1Dot8Dot79.php
+++ app/Migrations/V1Dot8Dot79.php
@@ -29,6 +29,33 @@ class V1Dot8Dot79 extends Base
             ->setParameter('true', true, ParameterType::BOOLEAN)
             ->executeQuery();
 
+        $this->getConnection()->createQueryBuilder()
+            ->update('export_configurator_item')
+            ->set('name', ':null')
+            ->where('type!=:fieldType')
+            ->setParameter('null', null, ParameterType::NULL)
+            ->setParameter('fieldType', 'Field')
+            ->executeQuery();
+
+        $res = $this->getConnection()->createQueryBuilder()
+            ->select('*')
+            ->from('export_configurator_item')
+            ->where('type=:fieldType')
+            ->setParameter('fieldType', 'Field')
+            ->fetchAllAssociative();
+
+        foreach ($res as $row) {
+            $this->getConnection()->createQueryBuilder()
+                ->delete('export_configurator_item')
+                ->where('export_feed_id=:exportFeedId')
+                ->andWhere('name=:name')
+                ->andWhere('id!=:id')
+                ->setParameter('exportFeedId', $row['export_feed_id'])
+                ->setParameter('name', $row['name'])
+                ->setParameter('id', $row['id'])
+                ->executeQuery();
+        }
+
         $this->exec("CREATE UNIQUE INDEX IDX_EXPORT_CONFIGURATOR_ITEM_UNIQUE_FIELD ON export_configurator_item (deleted, name, export_feed_id)");
     }
 
