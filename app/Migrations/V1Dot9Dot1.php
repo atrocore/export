38930a8d1b0608b45cbaeb5c6cd2a36282d26ad6
Kevin Teguia <kevin.teguia@atrocore.com>
2025-08-12 13:23:02 +0100

    taking in account language for attribute

--- app/Migrations/V1Dot9Dot1.php
+++ app/Migrations/V1Dot9Dot1.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 namespace Export\Migrations;
 
 use Atro\Core\Migration\Base;
+use Atro\Core\Utils\Util;
+use Doctrine\DBAL\ParameterType;
 
 class V1Dot9Dot1 extends Base
 {
@@ -34,11 +36,42 @@ class V1Dot9Dot1 extends Base
                 ->setParameter('field', 'Field')
                 ->setParameter('type', 'Attribute')
                 ->executeStatement();
+
+            $data = $this->getConnection()->createQueryBuilder()
+                ->select('eci.*, a.code as attribute_code')
+                ->from('export_configurator_item', 'eci')
+                ->leftJoin('eci', 'attribute', 'a', 'eci.entity_attribute_id = a.id')
+                ->where('eci.entity_attribute_id IS NOT NULL')
+                ->andWhere('eci.language IS NOT NULL AND eci.deleted = :false')
+                ->setParameter('false', false, ParameterType::BOOLEAN)
+                ->fetchAllAssociative();
+
+            foreach ($data as $row) {
+                if(empty($row['language']) || $row['language'] === 'main') {
+                    continue;
+                }
+                $name = $row['entity_attribute_id'];
+                if(!empty($row['attribute_code'])) {
+                    $name = preg_match('/^[a-zA-Z][a-zA-Z0-9_]*$/', $row['attribute_code']) === 1 ? $row['attribute_code'] : $name;
+                }
+
+                $name = $name.Util::toCamelCase(strtolower($row['language']), '_', true);
+                $this->getConnection()->createQueryBuilder()
+                    ->update('export_configurator_item')
+                    ->set('name', ':name')
+                    ->where('id = :id')
+                    ->setParameter('name', $name)
+                    ->setParameter('id', $row['id'])
+                    ->executeStatement();
+            }
         } catch (\Throwable $e) {
         }
 
+
+
+
         $this->exec("ALTER TABLE export_configurator_item DROP attribute_value");
-        $this->exec("ALTER TABLE export_configurator_item DROP \"language\"");
+//       $this->exec("ALTER TABLE export_configurator_item DROP \"language\"");
         $this->exec("ALTER TABLE export_configurator_item DROP fallback_language");
 //      $this->exec("ALTER TABLE export_configurator_item DROP attribute_id");
         $this->exec("DROP INDEX idx_import_configurator_item_attribute_id;");
