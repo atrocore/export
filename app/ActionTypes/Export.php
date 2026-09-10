48d648ea325962f72367253760acc4338beb5c6e
Roman Ratsun <roman.ratsun@atrocore.com>
2026-09-10 09:13:35 +0300

    Refactor: Move `exportFeedId` check earlier in `executeNow` method

--- app/ActionTypes/Export.php
+++ app/ActionTypes/Export.php
@@ -26,6 +26,10 @@ class Export extends AbstractAction
 
     public function executeNow(Entity $action, \stdClass $input): bool
     {
+        if (empty($action->get('exportFeedId'))) {
+            return false;
+        }
+
         $payload = empty($action->get('payload')) ? '' : (string)$action->get('payload');
         $templateData = [];
 
@@ -91,9 +95,6 @@ class Export extends AbstractAction
 
         /** @var \Export\Services\ExportFeed $service */
         $service = $this->getServiceFactory()->create('ExportFeed');
-        if (empty($action->get('exportFeedId'))) {
-            return false;
-        }
 
         $exportFeed = $service->getEntity($action->get('exportFeedId'));
         if (empty($exportFeed) || empty($exportFeed->get('isActive'))) {
