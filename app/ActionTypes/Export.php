66de758158c641424919aa267299b303d0a43fd4
Roman Ratsun <roman.ratsun@atrocore.com>
2026-09-10 09:12:35 +0300

    Fix: Prevent export attempt with missing `exportFeedId`

--- app/ActionTypes/Export.php
+++ app/ActionTypes/Export.php
@@ -91,7 +91,6 @@ class Export extends AbstractAction
 
         /** @var \Export\Services\ExportFeed $service */
         $service = $this->getServiceFactory()->create('ExportFeed');
-
         if (empty($action->get('exportFeedId'))) {
             return false;
         }
